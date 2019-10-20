3f9153b1d1dbc46d3479d6af49128cccc76defd7
thawatchai-p <53156869+thawatchai-p@users.noreply.github.com>
2019-10-20 14:57:09 +0700

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,18 @@
+library(dplyr)
+## Download the file with unzip
+FileURL = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+download.file(FileURL, destfile = "./NEI_data.zip")
+unzip(zipfile = "./NEI_data.zip")
+
+## Read the data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+NEI$year <- as.factor(NEI$year)
+df1 <- aggregate(data = NEI, Emissions ~ year, sum)
+barplot(height = df1$Emissions/1000000, names.arg = df1$year, 
+        xlab = "year", ylab = "Total PM2.5 (unit: million tons)",
+        main = "Total PM2.5 in the United States")
+
+## Copy plot3 to a PNG file
+dev.copy(png, file = "plot1.png")
+dev.off()
